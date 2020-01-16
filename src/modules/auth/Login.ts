9fca360a520578a641886bda29f2f35cc566720f
Mikołaj Rosowski <m.rosowski1@wp.pl>
2020-01-16 21:23:40 +0100

    Updated Login mutation to return user (with conditional teams)

--- src/modules/auth/Login.ts
+++ src/modules/auth/Login.ts
@@ -1,23 +1,30 @@
-import { Resolver, Mutation, Ctx, Args } from 'type-graphql';
+import { Resolver, Mutation, Ctx, Args, Info } from 'type-graphql';
 import { LoginInput } from './login/LoginInput';
 import { AppContext } from '../../@types/AppContext';
 import { User } from '../../entity/User';
 import { AuthenticationError } from 'apollo-server-express';
+import { doesPathExist } from '../../utils/doesPathExist';
+import { GraphQLResolveInfo } from 'graphql';
 
 @Resolver()
 export class LoginResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => User)
   async login(
     @Args() { email, password }: LoginInput,
-    @Ctx() ctx: AppContext
-  ): Promise<boolean> {
-    const user = await User.findOne({ where: { email } });
+    @Ctx() ctx: AppContext,
+    @Info() { fieldNodes }: GraphQLResolveInfo
+  ): Promise<User> {
+    const relations = doesPathExist(fieldNodes, ['login', 'teams'])
+      ? ['teams']
+      : [];
+
+    const user = await User.findOne({ where: { email }, relations });
 
     if (!user || !(await user.isPasswordValid(password))) {
       throw new AuthenticationError('invalid email or password');
     }
 
     ctx.req.session!.userId = user.id;
-    return true;
+    return user;
   }
 }
