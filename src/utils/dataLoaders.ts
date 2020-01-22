5315c4ccbd473be096fcdfa041d5ccc2af171872
Mikołaj Rosowski <m.rosowski1@wp.pl>
2020-01-22 15:53:30 +0100

    Created registerNewDataLoader util & all app datatypes loaders

--- src/utils/dataLoaders.ts
+++ src/utils/dataLoaders.ts
@@ -1,23 +1,60 @@
 import { DataLoaderFactory } from 'dataloader-factory';
 import { GraphQLResolveInfo } from 'graphql';
+import { In, BaseEntity } from 'typeorm';
 import { loadRelations } from './loadRelations';
 import { Team } from '../entity/Team';
-import { In } from 'typeorm';
+import { User } from '../entity/User';
+import { Channel } from '../entity/Channel';
+import { Message } from '../entity/Message';
 
-DataLoaderFactory.registerFiltered<number, Team>('team', {
-  fetch: async (ids, info: GraphQLResolveInfo) => {
-    const relations = loadRelations({
-      info,
-      paths: ['channels', 'members', 'owner']
-    });
-    const teams = await Team.find({
-      where: { id: In(ids) },
-      loadRelationIds: { relations }
-    });
-    return teams;
-  },
-  returnOne: true,
-  extractKey: team => team.id
+const registerNewDataLoader = <T extends typeof BaseEntity>({
+  name,
+  Model,
+  paths
+}: {
+  name: string;
+  Model: T;
+  paths: string[];
+}) => {
+  DataLoaderFactory.registerFiltered<number>(name, {
+    fetch: async (ids, info: GraphQLResolveInfo) => {
+      const relations = loadRelations({
+        info,
+        paths
+      });
+      const data = await Model.find({
+        where: { id: In(ids) },
+        loadRelationIds: { relations }
+      });
+      return data;
+    },
+    returnOne: true,
+    extractKey: data => data.id
+  });
+};
+
+registerNewDataLoader({
+  name: 'team',
+  Model: Team,
+  paths: ['channels', 'members', 'owner']
+});
+
+registerNewDataLoader({
+  name: 'user',
+  Model: User,
+  paths: ['teams']
+});
+
+registerNewDataLoader({
+  name: 'channel',
+  Model: Channel,
+  paths: ['messages', 'members']
+});
+
+registerNewDataLoader({
+  name: 'message',
+  Model: Message,
+  paths: ['channel', 'author']
 });
 
 export { DataLoaderFactory };
